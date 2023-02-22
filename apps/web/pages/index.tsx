c39da7bf5262af9fbe96127f2df07cc45cbacc6d
illia-om <illia.omelchenko@icloud.com>
2023-02-22 00:02:48 +0000

    add core Box component

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,11 +1,13 @@
-import { Text } from "@cellar-door/ui";
+import { Text, Box } from "@cellar-door/ui";
 
 export default function Web() {
   return (
-    <div>
+    <Box padding={'base'} maxWidth='containerMedium' margin='auto'>
+      <Box display={'flex'} flexDirection='column' gap='small'>
       <Text>my name is illia</Text>
       <Text>and this is my first take</Text>
       <Text>on typography</Text>
-    </div>
+      </Box>
+    </Box>
   );
 }
---
+++ packages/ui/src/core/Box/Box.tsx
@@ -0,0 +1,6 @@
+import { createBox } from '@dessert-box/react';
+import { sprinkles } from '../../style/sprinkles.css';
+
+const StyledBox = createBox({atoms:sprinkles});
+
+export const Box = StyledBox;
